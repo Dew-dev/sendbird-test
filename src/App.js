50e8fc53a0ba16fe8b550f27c3a8f07c834468aa
dewa <dewdewd22@gmail.com>
2024-02-27 13:12:53 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,149 @@
 // import SendbirdApp from '@sendbird/uikit-react/App';
 import SendbirdProvider from "@sendbird/uikit-react/SendbirdProvider";
 import MessageSearch from "@sendbird/uikit-react/MessageSearch";
+import {
+  GroupChannelFilter,
+  GroupChannelListOrder,
+  MessageFilter,
+  MessageCollectionInitPolicy,
+} from "@sendbird/chat/groupChannel";
+import { useState, useRef } from "react";
 import "@sendbird/uikit-react/dist/index.css";
 import SendbirdChat from "@sendbird/chat";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 import Custom from "./custom";
 import CustomEvents from "./custom";
 import { GroupChannelModule } from "@sendbird/chat/groupChannel";
+let sb;
 
 function App() {
   const APP_ID = "FFA3FB52-75AC-44EA-B991-8E57D7B5C8BE";
-  const USER_ID = "a";
-  const NICKNAME = "Joko";
+  // const USER_ID = "a";
+  // const NICKNAME = "Joko";
+  const [state, updateState] = useState({
+    applicationUsers: [],
+    groupChannelMembers: [],
+    currentlyJoinedChannel: null,
+    messages: [],
+    channels: [],
+    messageInputValue: "",
+    userNameInputValue: "",
+    userIdInputValue: "a",
+    channelNameUpdateValue: "",
+    settingUpUser: true,
+    file: null,
+    messageToUpdate: null,
+    messageCollection: null,
+    loading: false,
+    error: false,
+  });
+  const stateRef = useRef();
+  stateRef.current = state;
 
-  const sb = SendbirdChat.init({
-    appId: APP_ID,
-    modules : [new GroupChannelModule()],
-  })
-  sb.connect(USER_ID);
+  const channelRef = useRef();
+  const channelHandlers = {
+    onChannelsAdded: (context, channels) => {
+      const updatedChannels = [...channels, ...stateRef.current.channels];
+      updateState({
+        ...stateRef.current,
+        channels: updatedChannels,
+        applicationUsers: [],
+      });
+    },
+    onChannelsDeleted: (context, channels) => {
+      const updatedChannels = stateRef.current.channels.filter((channel) => {
+        return !channels.includes(channel.url);
+      });
+      updateState({ ...stateRef.current, channels: updatedChannels });
+    },
+    onChannelsUpdated: (context, channels) => {
+      const updatedChannels = stateRef.current.channels.map((channel) => {
+        const updatedChannel = channels.find(
+          (incomingChannel) => incomingChannel.url === channel.url
+        );
+        if (updatedChannel) {
+          return updatedChannel;
+        } else {
+          return channel;
+        }
+      });
+
+      updateState({ ...stateRef.current, channels: updatedChannels });
+    },
+  };
+  // const sb = SendbirdChat.init({
+  //   appId: APP_ID,
+  //   modules: [new GroupChannelModule()],
+  // });
+  // sb.connect(USER_ID);
+  // const setupUser = async () => {
+  //   const { userNameInputValue, userIdInputValue } = state;
+  //   const sendbirdChat = await SendbirdChat.init({
+  //     appId: APP_ID,
+  //     localCacheEnabled: true,
+  //     modules: [new GroupChannelModule()],
+  //   });
+
+  //   await sendbirdChat.connect(userIdInputValue);
+  //   await sendbirdChat.setChannelInvitationPreference(true);
+
+  //   const userUpdateParams = {};
+  //   userUpdateParams.nickname = userNameInputValue;
+  //   userUpdateParams.userId = userIdInputValue;
+  //   await sendbirdChat.updateCurrentUserInfo(userUpdateParams);
+
+  //   sb = sendbirdChat;
+  //   updateState({ ...state, loading: true });
+  //   const [channels, error] = await loadChannels(channelHandlers);
+  //   if (error) {
+  //     return onError(error);
+  //   }
+  //   const onError = (error) => {
+  //     updateState({ ...state, error: error.message });
+  //     console.log(error);
+  //   };
+  //   updateState({
+  //     ...state,
+  //     channels: channels,
+  //     loading: false,
+  //     settingUpUser: false,
+  //   });
+  // };
+  const params = {
+    invitedUserIds: ["a", "6e336b1f-6505-4f46-ae27-ee62d9529580"],
+    name: "Konsultasi",
+    isDistinct: false,
+  };
+  const channel = async () => await sb.groupChannel.createChannel(params);
+  // console.log(channel);
   return (
     <div className="App">
       <SendbirdProvider
         appId={APP_ID}
-        userId={USER_ID}
+        userId={state.userIdInputValue}
         isMentionEnabled
         isVoiceMessageEnabledS
       >
-        <Custom sb={sb}/>
-        
+        <Custom sb={sb} />
       </SendbirdProvider>
     </div>
   );
 }
+
+const loadChannels = async (channelHandlers) => {
+  const groupChannelFilter = new GroupChannelFilter();
+  groupChannelFilter.includeEmpty = true;
+
+  const collection = sb.groupChannel.createGroupChannelCollection({
+    filter: groupChannelFilter,
+    order: GroupChannelListOrder.LATEST_LAST_MESSAGE,
+  });
+
+  collection.setGroupChannelCollectionHandler(channelHandlers);
+
+  const channels = await collection.loadMore();
+  return [channels, null];
+};
 
 export default App;
--- src/custom.jsx
+++ src/custom.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 // import CustomSearchItem from "@sendbird/uikit-react";
 // import SendbirdChat from "@sendbird/chat";
-import { ChannelList, Channel, ChannelSettings, SendBirdProvider } from "@sendbird/uikit-react";
+import {
+  ChannelList,
+  Channel,
+  ChannelSettings,
+  SendBirdProvider,
+} from "@sendbird/uikit-react";
 // import Channel from '@sendbird/uikit-react/Channel';
-import VoiceMessageInput from '@sendbird/uikit-react/ui/VoiceMessageInput';
+import VoiceMessageInput from "@sendbird/uikit-react/ui/VoiceMessageInput";
 // import CustomizedChatInput from './CustomizedMessageInput';
 
 // const sb = SendbirdChat.init({
@@ -13,18 +18,18 @@ import VoiceMessageInput from '@sendbird/uikit-react/ui/VoiceMessageInput';
 //   ],
 // });
 
-function Custom({sb}) {
+function Custom({ sb }) {
   const [currentChannel, setCurrentChannel] = useState(null);
   const currentChannelUrl = currentChannel ? currentChannel.url : "";
   const [showSettings, setShowSettings] = useState(false);
   var channelChatDiv = document.getElementsByClassName("channel-chat")[0];
-  var addGroupButton = document.getElementsByClassName("sendbird-iconbutton")[0];
+  var addGroupButton = document.getElementsByClassName(
+    "sendbird-iconbutton"
+  )[0];
   // // // var channelChatDiv = document.getElementsById("channel-chat");
-  try{
-    addGroupButton.style.display = 'none';
-  } catch(e){
-
-  }
+  // try {
+  //   addGroupButton.style.display = "none";
+  // } catch (e) {}
   const renderSettingsBar = () => {
     channelChatDiv.style.width = "52%";
     channelChatDiv.style.cssFloat = "left";
@@ -51,17 +56,15 @@ function Custom({sb}) {
             renderSettingsBar();
           }}
           // renderMessageInput={() => {
-            
+
           // }}
         />
       </div>
       {showSettings && (
         <div className="channel-settings">
-          
           <ChannelSettings
             channelUrl={currentChannelUrl}
             onCloseClick={() => {
-              
               setShowSettings(false);
               hideSettingsBar();
               // testclick();
@@ -69,12 +72,9 @@ function Custom({sb}) {
           />
         </div>
       )}
-      <div>
-        
-      </div>
+      <div></div>
     </div>
   );
 }
-
 
 export default Custom;
